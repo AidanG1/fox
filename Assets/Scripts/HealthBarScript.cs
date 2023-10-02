768851a0e91a7238f9f84267ba59c4b246330e37
Joey Stanley <joseph.stanley@duke.edu>
2023-10-02 16:48:17 +0200

    Added Health Bar to LevelScene
    Changed PlayerController, HealthBarScript, and made health bar

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
